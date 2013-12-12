26a6d3d3b47222d891a4f2da8ddbdf8151036d85
Scott Travis <scott.w.travis@gmail.com>
2013-12-11 20:58:13 -0600

    Fix vendor name.

--- src/Travis/CLI/Provider.php
+++ src/Travis/CLI/Provider.php
@@ -20,7 +20,7 @@ class Provider extends ServiceProvider {
      */
     public function boot()
     {
-        $this->package('swt83/cli', 'travis/cli');
+        $this->package('travis/cli');
     }
 
     /**
