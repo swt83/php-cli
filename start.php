52995c8ebfd438af1489dbc7025bf4bb0c6b5493
Scott Travis <scott.w.travis@gmail.com>
2014-01-02 15:27:30 -0600

    Removed legacy file.

--- start.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-#################################################
-# Legacy autoloader for Laravel 3.
-#################################################
-Autoloader::map(array(
-    'CLI' => __DIR__.'/src/Travis/CLI.php',
-));
